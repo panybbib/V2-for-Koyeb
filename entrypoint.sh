6a543cb73d4a9cd519e196d88ea349414ee7e04a
panybbib <pan.107@163.com>
2023-08-27 14:09:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 base64 -d config > config.json
 UUID=${UUID:-'668796c3-69d4-4e60-821b-0d1d631c477b'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/v2ms'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/v2ls'}
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" config.json
 sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/nginx/nginx.conf
 
